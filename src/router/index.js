1328227891b8011945eb19b98d64e81e75996078
yaojun <yaojun012022@163.com>
2022-08-13 10:53:36 +0800

    使用webpack把组件按组分块

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '@/views/Login'
-import Layout from '@/views/Layout'
-import Home from '@/views/Home'
-import User from '@/views/User'
-import Search from '@/views/Search'
-import SearchResult from '@/views/Search/SearchResult.vue'
-import ArticleDetail from '@/views/ArticleDetail'
-import UserEdit from '@/views/User/UserEdit.vue'
-import UserChat from '@/views/Chat'
+const Login = () => import(/* webpackChunkName: "Login" */ '@/views/Login')
+const Layout = () => import(/* webpackChunkName: "Layout" */ '@/views/Layout')
+const Home = () => import(/* webpackChunkName: "Home" */ '@/views/Home')
+const User = () => import(/* webpackChunkName: "User" */ '@/views/User')
+const Search = () => import(/* webpackChunkName: "Search" */ '@/views/Search')
+const SearchResult = () => import(/* webpackChunkName: "SearchResult" */ '@/views/Search/SearchResult.vue')
+const ArticleDetail = () => import(/* webpackChunkName: "ArticleDetail" */ '@/views/ArticleDetail')
+const UserEdit = () => import(/* webpackChunkName: "UserEdit" */ '@/views/User/UserEdit.vue')
+const UserChat = () => import(/* webpackChunkName: "UserChat" */ '@/views/Chat')
+
+// import Login from '@/views/Login'
+// import Layout from '@/views/Layout'
+// import Home from '@/views/Home'
+// import User from '@/views/User'
+// import Search from '@/views/Search'
+// import SearchResult from '@/views/Search/SearchResult.vue'
+// import ArticleDetail from '@/views/ArticleDetail'
+// import UserEdit from '@/views/User/UserEdit.vue'
+// import UserChat from '@/views/Chat'
 
 Vue.use(VueRouter)
 
